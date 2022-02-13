9ab3ea03a0c34df88587a52ed2f6b630357ae2ba
Yordan Lyubenov <yordan.lyubenov@accedia.com>
2022-02-13 16:29:45 +0200

    Initial Commit

---
+++ SwiftUITemplate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITemplate
+//
+//  Created by Yordan Lyubenov on 13.02.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUITemplate/SwiftUITemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITemplateApp.swift
+//  SwiftUITemplate
+//
+//  Created by Yordan Lyubenov on 13.02.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
